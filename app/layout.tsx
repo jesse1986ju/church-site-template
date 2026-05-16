794ba03f852fc0140747e2ea78225f8841c27390
jesse1986ju <jesse@engagesolutionsok.com>
2026-05-16 13:02:16 -0500

    fix: add edge runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
 
+export const runtime = 'edge';
+
 export async function generateMetadata(): Promise<Metadata> {
   const churchName = process.env.CHURCH_NAME ?? 'Our Church';
   return {
